6543c7e5a36c0dd7177328646e2e927080c057bf
jovi <kwong.jovi@gmail.com>
2022-11-29 23:51:02 +0000

    feat(HangmanWord): add and set up main div

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,17 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "hello";
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "san-serif",
+      }}
+    >
+      {word}
+    </div>
+  );
+}

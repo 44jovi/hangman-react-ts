76db578371e360a57480721e5ddd6bbc4ccb272f
jovi <kwong.jovi@gmail.com>
2022-11-29 17:06:58 +0000

    feat(App): set up array for letters guessed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import words from "./word-list.json";
 
 function App() {
+  // Word to be guessed
   const [wordToGuess, setWordToGuess] = useState(() => {
     // Random word from existing word list
     return words[Math.floor(Math.random() * words.length)];
   });
-  console.log(wordToGuess);
+
+  // Letters guessed - set type to array of strings
+  const [lettersGuessed, setLettersGuessed] = useState<string[]>([]);
   return <h1>Welcome!</h1>;
 }
 

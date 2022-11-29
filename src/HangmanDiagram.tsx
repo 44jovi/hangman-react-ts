75cb393dc63f69da6c38f9c84641a21ae1ee482c
jovi <kwong.jovi@gmail.com>
2022-11-29 22:45:35 +0000

    feat(HangmanDiagram): add diagram of hangman frame (inline CSS)

--- src/HangmanDiagram.tsx
+++ src/HangmanDiagram.tsx
@@ -1,1 +1,40 @@
-export function HangmanDiagram() {}
+export function HangmanDiagram() {
+  // Main div
+  return (
+    <div style={{ position: "relative" }}>
+      {/* Small vertical bar */}
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      ></div>
+      {/* Top horizontal bar */}
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      ></div>
+      {/* Central vertical bar */}
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      ></div>
+      <div
+        style={{ height: "10px", width: "250px", background: "black" }}
+      ></div>
+    </div>
+  );
+}

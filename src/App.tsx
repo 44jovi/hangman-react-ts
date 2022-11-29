10b03798b0456d48df51b6bd5512fc93148826f9
jovi <kwong.jovi@gmail.com>
2022-11-29 16:54:27 +0000

    feat(App): add function to return random word from word-list.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import words from "./word-list.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    // Random word from existing word list
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  console.log(wordToGuess);
   return <h1>Welcome!</h1>;
 }
+
 export default App;

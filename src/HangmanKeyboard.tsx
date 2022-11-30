adaf9502710296dbae5c51d50f396fbce7440ef7
jovi <kwong.jovi@gmail.com>
2022-11-30 11:36:06 +0000

    feat(HangmanKeyboard): add function. Return HTML buttons of all letters

--- src/HangmanKeyboard.tsx
+++ src/HangmanKeyboard.tsx
@@ -1,1 +1,44 @@
-export function HangmanKeyboard() {}
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export function HangmanKeyboard() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat (auto-fit, minmax(75px, 1fr",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        return <button key={key}>{key}</button>;
+      })}
+    </div>
+  );
+}

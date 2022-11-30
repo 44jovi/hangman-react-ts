c860d3bf73bdb8cb3ccd3f971414ea07676be44f
jovi <kwong.jovi@gmail.com>
2022-11-30 12:05:26 +0000

    fix(HangmanKeyboard): fix styling to achieve keyboard grid to fill viewport

--- src/HangmanKeyboard.tsx
+++ src/HangmanKeyboard.tsx
@@ -32,7 +32,7 @@ export function HangmanKeyboard() {
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "repeat (auto-fit, minmax(75px, 1fr",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
         gap: ".5rem",
       }}
     >
